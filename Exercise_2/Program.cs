9fa519d859dccb54867a38f4a61f658c1ba47f44
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-12-12 01:39:10 +0300

    Tests added to Exercise_2

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,9 +1,29 @@
-﻿Random rnd = new Random();
+﻿TestingGetSumOfOddIndex();
+Random rnd = new Random();
 int randomSizeOfArray = rnd.Next(1, 20);
 int[] array = new int [randomSizeOfArray];
 FillArray(array);
 PrintArray(array);
 Console.WriteLine($" -> {GetSumOfOddIndex(array)}");
+
+
+void TestingGetSumOfOddIndex()
+{
+    Console.WriteLine("Testing of the \"GetSumOfOddIndex\" method has been launched... ");
+    int[] testArray = {3, 7, 23, 12};
+    int expected = 19;
+    int actual = GetSumOfOddIndex(testArray);
+    bool isEqual = expected == actual;
+    if (isEqual)
+    {
+        Console.WriteLine("Test completed successfully!");
+    }
+    else
+    {
+        Console.WriteLine("Error! Need to fix the method!");
+    }
+    Console.WriteLine();
+}
 
 
 int GetSumOfOddIndex(int[] array)
