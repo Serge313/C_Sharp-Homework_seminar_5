f3dda67e4a16ec529c0538cef328ac0c94382c79
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-12-12 01:43:58 +0300

    Tests added to Exercise_3

--- Exercise_3/Program.cs
+++ Exercise_3/Program.cs
@@ -1,9 +1,29 @@
-﻿Random random = new Random();
+﻿TestingFindDifferenceBetweenMaxAndMinNumber();
+Random random = new Random();
 int randomSizeOfArray = random.Next(3, 10);
 int[] array = new int [randomSizeOfArray];
 FillArray(array);
 PrintArray(array);
 Console.WriteLine($" -> {FindDifferenceBetweenMaxAndMinNumber(array)}");
+
+
+void TestingFindDifferenceBetweenMaxAndMinNumber()
+{
+    Console.WriteLine("Testing of the \"FindDifferenceBetweenMaxAndMinNumber\" method has been launched... ");
+    int[] testArray = {3, 7, 22, 2, 78};
+    int expected = 76;
+    int actual = FindDifferenceBetweenMaxAndMinNumber(testArray);
+    bool isEqual = expected == actual;
+    if (isEqual)
+    {
+        Console.WriteLine("Test completed successfully!");
+    }
+    else
+    {
+        Console.WriteLine("Error! Need to fix the method!");
+    }
+    Console.WriteLine();
+}
 
 
 int FindDifferenceBetweenMaxAndMinNumber(int[] array)
