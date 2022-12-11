1ca4f792f6a16aadf83513e8138026490bbbef6d
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-12-11 05:47:16 +0300

    Exercise_1 completed

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,62 @@
+﻿int sizeOfArray = GetNumber("Enter size of your array ");
+int[] array = new int [sizeOfArray];
+FillArray(array);
+PrintArray(array);
+int count = GetNumberOfEvens(array);
+Console.WriteLine($" -> {count}");
+
+
+int GetNumberOfEvens(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write(array[i]);
+        }
+    }
+}
+
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+    if (isParsed)
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Ivalid value entered!");
+        return -1;
+    }
+}
